e1de1622fe074408d655ca9cc0bca2a332187c65
Sarbo Roy <sarbo.roy@infinitus.ai>
2023-12-02 02:09:54 -0800

    fe changes

--- components/Chatbar/components/ChatbarSettings.tsx
+++ components/Chatbar/components/ChatbarSettings.tsx
@@ -42,14 +42,6 @@ export const ChatbarSettings = () => {
         <ClearConversations onClearConversations={handleClearConversations} />
       ) : null}
 
-      <Import onImport={handleImportConversations} />
-
-      <SidebarButton
-        text={t('Export data')}
-        icon={<IconFileExport size={18} />}
-        onClick={() => handleExportData()}
-      />
-
       <SidebarButton
         text={t('Settings')}
         icon={<IconSettings size={18} />}
@@ -59,8 +51,6 @@ export const ChatbarSettings = () => {
       {!serverSideApiKeyIsSet ? (
         <Key apiKey={apiKey} onApiKeyChange={handleApiKeyChange} />
       ) : null}
-
-      {!serverSidePluginKeysSet ? <PluginKeys /> : null}
 
       <SettingDialog
         open={isSettingDialogOpen}
--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -360,7 +360,7 @@ const Home = ({
       }}
     >
       <Head>
-        <title>Chatbot UI</title>
+        <title>Infinitus Chat</title>
         <meta name="description" content="ChatGPT but better." />
         <meta
           name="viewport"
